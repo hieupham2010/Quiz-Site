ef13536f528697acdf582e538aa5a3ca673fe710
hieupham2010 <01684673046h@gmail.com>
2021-02-17 15:42:51 +0700

    add quiz page

--- Main.py
+++ Main.py
@@ -4,8 +4,11 @@ app = Flask(__name__ , template_folder='Views')
 app.secret_key = b'HieuPham-518H0501'
 @app.route('/' , methods=['GET'])
 def Home():
-    if request.method == 'GET' and 'username' in session:
-        return render_template('Home/Index.html')
+    if request.method == 'GET' and 'id' in session:
+        query = 'SELECT * FROM subjects WHERE SubjectID IN (SELECT SubjectID FROM members WHERE AccountID = %s)'
+        val = (session['id'])
+        data = conn.executeQueryValData(query , val)
+        return render_template('Home/Index.html' , data=data)
     return redirect(url_for('Login'))
 
 @app.route('/Login', methods=['POST' , 'GET'])
@@ -19,7 +22,7 @@ def Login():
         data = conn.executeQueryValData(query , val)
         if data != False:
             if len(data) == 1:
-                session['username'] = form['username']
+                session['id'] = data[0][0]
                 return redirect(url_for('Home'))
             else:
                 flash('Invalid username or password please try again')
@@ -27,6 +30,17 @@ def Login():
         else: return "Something went wrong"
     return "Unsupported method"
 
+@app.route('/Quiz/<id>')
+def Quiz(id):
+    query = 'SELECT * FROM members WHERE SubjectID = %s AND AccountID = %s'
+    val = (id , session['id'])
+    data = conn.executeQueryValData(query , val)
+    if len(data) > 0:
+        query = 'SELECT * FROM questions WHERE SubjectID = %s'
+        val = (id)
+        data = conn.executeQueryValData(query , val)
+        return render_template('Home/Quiz.html' , data=data)
+    return redirect(url_for('Home'))
 
 if __name__ == "__main__":
     app.run(debug=True)
